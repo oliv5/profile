cd652a77b3477d6b1e672142bf7eaa3580c257ec
Olivier Lanneluc <olanneluc@sequans.com>
2022-08-07 11:01:28 +0200

    [desktop] fix files: not executable, exec path must exist, use /bin/sh -c to use $HOME

--- bin/profile/mount-private.sh
+++ bin/profile/mount-private.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
-( . "$RC_DIR/.rc.d/20_mount.sh"
-mount_private_ecryptfs
+( set -e
+    . "${RC_DIR:-$HOME/.rc.d}/.rc.d/20_mount.sh"
+    if mountpoint "$HOME/private"; then
+	umount_private_ecryptfs
+	echo -e "Press enter..."
+	read _
+    else
+	mount_private_ecryptfs
+    fi
 )
