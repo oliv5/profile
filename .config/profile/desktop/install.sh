ab2814a25cc62bc7046f93eb99a8e12f8745bf18
Oliv5 <4175562+oliv5@users.noreply.github.com>
2022-08-07 11:01:28 +0200

    [desktop] fix files: not executable, exec path must exist, use /bin/sh -c to use $HOME

--- .config/profile/desktop/install.sh
+++ .config/profile/desktop/install.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 SRC="${1:-$PWD}"
 for FILE in "${SRC}"/*.desktop; do
-    ln -fsv "${FILE}" "${HOME}/.local/share/applications/$(basename "${FILE}")"
-    chmod +x "${FILE}"
+    [ -e "${FILE}" ] && 
+        ln -fsv "${FILE}" "${HOME}/.local/share/applications/$(basename "${FILE}")"
 done
 for FILE in "${SRC}"/*.png "${SRC}"/*.ico; do
-    ln -fsv "${FILE}" "${HOME}/.local/share/icons/$(basename "${FILE}")"
+    [ -e "${FILE}" ] && 
+        ln -fsv "${FILE}" "${HOME}/.local/share/icons/$(basename "${FILE}")"
 done
 
