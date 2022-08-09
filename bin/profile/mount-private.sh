ffa55d88566aa4b2d81ee41d0b2706dc55b3e43d
Olivier Lanneluc <olanneluc@sequans.com>
2022-08-09 10:28:07 +0200

    [private] ask question

--- bin/profile/mount-private.sh
+++ bin/profile/mount-private.sh
@@ -2,9 +2,9 @@
 ( set -e
     . "${RC_DIR:-$HOME/.rc.d}/.rc.d/20_mount.sh"
     if mountpoint "$HOME/private"; then
-	umount_private_ecryptfs
-	echo -e "Press enter..."
+	echo -n "You are going to unmount your private stash (enter/ctrl-c)..."
 	read _
+	umount_private_ecryptfs
     else
 	mount_private_ecryptfs
     fi
