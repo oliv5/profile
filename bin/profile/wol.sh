ca37cd610a748dbaacfa486f5ab74d4b9174e558
Oliv5 <4175562+oliv5@users.noreply.github.com>
2022-08-10 14:41:35 +0200

    [bin] fix wol script, add usage, simplify profile information retrieval

--- bin/profile/wol.sh
+++ bin/profile/wol.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-PROFILE="$(echo $1 | tr '[:lower:]' '[:upper:]')"
-METHOD="$2"
-shift $(($# < 2 ? $# : 2))
 
 # Returns true when a given method exists and is selected
 _exists() {
@@ -16,17 +13,12 @@ _run() {
 
 # Extract some parameters
 _get_wol_params() {
-  if [ -n "$(eval echo \${${PROFILE}_MAC})" ]; then
-    eval MAC="\${${PROFILE}_MAC}"
-    eval IP="\${${PROFILE}_IP}"
-    eval PORT="\${${PROFILE}_PORT}"
-  else
-    # Convert '-' into ':'
-    MAC="$(echo ${1:?No MAC address defined...} | tr '-' ':' | tr '[:lower:]' '[:upper:]')"
-    IP="${2:-255.255.255.255}"
-    PORT="${3:-9}"
-  fi
+  eval MAC="${1:-\${${PROFILE}_MAC:?No MAC address defined...}}"
+  eval IP="${2:-\${${PROFILE}_IP:-255.255.255.255}}"
+  eval PORT="${3:-\${${PROFILE}_PORT:-9}}"
   ITF="${4:-eth0}"
+  # Convert '-' into ':'
+  MAC="$(echo $MAC | tr '-' ':' | tr '[:lower:]' '[:upper:]')"
 }
 _get_ssh_params() {
   SSH_CREDS="${1:?No SSH host/credentials specified...}"
@@ -39,34 +31,48 @@ _urls() {
 }
 
 # Execute the WOL command
-if _exists wakeonlan "$METHOD"; then
-  _get_wol_params "$@"
-  _run wakeonlan -i "${IP}" -p "${PORT}" "${MAC}";
-elif _exists wol "$METHOD"; then
-  _get_wol_params "$@"
-  _run wol -i "${IP}" -p "${PORT}" "${MAC}"
-elif _exists etherwake "$METHOD"; then
-  _get_wol_params "$@"
-  _run etherwake -i "${ITF}" -b "${MAC}"
-elif _exists ether-wake "$METHOD"; then
-  _get_wol_params "$@"
-  _run ether-wake -i "${ITF}" -b "${MAC}"
-elif _exists curl "$METHOD"; then
-  _get_wol_params "$@"
-  for URL in $(_urls); do
-    _run curl -qs "${URL}" >/dev/null && break
-  done
-elif _exists wget "$METHOD"; then
-  _get_wol_params "$@"
-  for URL in $(_urls); do
-    _run wget --timeout=10 -q -O /dev/null "${URL}" && break
-  done
-elif _exists ssh "$METHOD"; then
-  SSH_CREDS="${1:?No SSH host/credentials specified...}"
-  METHOD="$2"; [ "$METHOD" = "ssh" ] && METHOD=""
+_execute_wol() {
+  local PROFILE="$(echo $1 | tr '[:lower:]' '[:upper:]')"
+  local METHOD="$2"
   shift $(($# < 2 ? $# : 2))
-  ssh "$SSH_CREDS" -- sh -c ':; . "$HOME/bin/profile/wol.sh" "$@"' "$PROFILE" "$METHOD" "$@"
-else
-  echo >&2 "No appropriate WOL method available..."
+
+  if _exists wakeonlan "$METHOD"; then
+    _get_wol_params "$@"
+    _run wakeonlan -i "${IP}" -p "${PORT}" "${MAC}";
+  elif _exists wol "$METHOD"; then
+    _get_wol_params "$@"
+    _run wol -i "${IP}" -p "${PORT}" "${MAC}"
+  elif _exists etherwake "$METHOD"; then
+    _get_wol_params "$@"
+    _run etherwake -i "${ITF}" -b "${MAC}"
+  elif _exists ether-wake "$METHOD"; then
+    _get_wol_params "$@"
+    _run ether-wake -i "${ITF}" -b "${MAC}"
+  elif _exists curl "$METHOD"; then
+    _get_wol_params "$@"
+    for URL in $(_urls); do
+      _run curl -qs "${URL}" >/dev/null && break
+    done
+  elif _exists wget "$METHOD"; then
+    _get_wol_params "$@"
+    for URL in $(_urls); do
+      _run wget --timeout=10 -q -O /dev/null "${URL}" && break
+    done
+  elif _exists ssh "$METHOD"; then
+    local SSH_CREDS="${1:?No SSH host/credentials specified...}"
+    local METHOD="$2"; [ "$METHOD" = "ssh" ] && METHOD=""
+    shift $(($# < 2 ? $# : 2))
+    ssh "$SSH_CREDS" -- sh -c ':; . "$HOME/bin/profile/wol.sh" "$@"' "$PROFILE" "$METHOD" "$@"
+  else
+    echo >&2 "No appropriate WOL method available..."
+    false
+  fi
+}
+
+# Main
+if [ $# -eq 0 ]; then
+  echo >&2 "Usage: wol.sh <profile> <method> [method params...]"
   false
+else
+  _execute_wol "$@"
 fi
