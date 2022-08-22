9f6b85f771c60586f7b194b30ff7be9a46ecf2b1
Oliv5 <oliv5@caramail.com>
2022-08-22 11:45:19 +0200

    [bin] add usbguard wrapper script

---
+++ bin/profile/usbguard.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+# https://wiki.archlinux.org/title/USBGuard
+
+usbguarg_block() {
+    sudo usbguard set-parameter ImplicitPolicyTarget block
+}
+
+usbguarg_allow() {
+    sudo usbguard set-parameter ImplicitPolicyTarget allow
+}
+
+usbguarg_record() {
+    sudo sh -c 'usbguard generate-policy > /etc/usbguard/rules.conf'
+}
+
+usbguard_status() {
+    sudo systemctl status usbguard
+    sudo systemctl status usbguard-dbus
+}
+
+usbguard_disable() {
+    sudo systemctl start usbguard
+    sudo systemctl start usbguard-dbus
+    usbguarg_allow
+    sudo systemctl stop usbguard
+    sudo systemctl stop usbguard-dbus
+    sudo systemctl disable usbguard
+    sudo systemctl disable usbguard-dbus
+}
+
+usbguard_enable() {
+    sudo systemctl enable usbguard
+    sudo systemctl enable usbguard-dbus
+}
+
+# Main
+if [ -n "$1" ]; then
+    "usbguard_$@"
+fi
