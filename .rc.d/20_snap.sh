ba27dba4e600404ba198726da4d25ded47fb6b69
Oliv5 <4175562+oliv5@users.noreply.github.com>
2022-09-02 12:26:15 +0200

    [rc] snap doesnt set -eu in the current shell

--- .rc.d/20_snap.sh
+++ .rc.d/20_snap.sh
@@ -10,11 +10,12 @@ snap_allow_home() {
 # Remove disabled snap
 # https://www.linuxuprising.com/2019/04/how-to-remove-old-snap-versions-to-free.html
 snap_rm_disabled() {
-  set -eu
+  ( set -eu
   LANG=en_US.UTF-8 snap list --all | awk '/disabled/{print $1, $3}' |
-    while read snapname revision; do
-      snap remove "$snapname" --revision="$revision"
+    while read NAME REV; do
+      sudo snap remove "$NAME" --revision="$REV"
     done
+  )
 }
 
 # Set number of retained snap versions of a package
