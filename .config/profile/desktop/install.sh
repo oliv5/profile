5c9e2a078681c219779fbac751e7f95d1cef1004
Oliv5 <4175562+oliv5@users.noreply.github.com>
2022-12-16 12:44:02 +0100

    [desktop] fix icons, no extension name + must use full path to force a local icon, so we must still use an install script

--- .config/profile/desktop/install.sh
+++ .config/profile/desktop/install.sh
@@ -1,11 +1,27 @@
 #!/bin/sh
 SRC="${1:-$PWD}"
-for FILE in "${SRC}"/*.desktop; do
-    [ -e "${FILE}" ] && 
-        ln -fsv "${FILE}" "${HOME}/.local/share/applications/$(basename "${FILE}")"
-done
+
 for FILE in "${SRC}"/*.png "${SRC}"/*.ico; do
-    [ -e "${FILE}" ] && 
-        ln -fsv "${FILE}" "${HOME}/.local/share/icons/$(basename "${FILE}")"
+    DST="${HOME}/.local/share/icons/$(basename "${FILE}")"
+    if [ -e "${FILE}" ]; then
+        ln -fsv "${FILE}" "${DST}"
+    fi
 done
 
+for FILE in "${SRC}"/*.desktop; do
+    DST="${HOME}/.local/share/applications/$(basename "${FILE}")"
+    if [ -e "${FILE}" ]; then
+        ln -fsv "${FILE}" "${DST}.link"
+
+        [ -e "${DST}" ] && rm -v "${DST}"
+        cp -v "${FILE}" "${DST}"
+        
+        ICON="$(awk -F= '/Icon=/{print $2}' "${DST}")"
+        ICON="${HOME}/.local/share/icons/$ICON"
+        [ -e "${ICON}.ico" ] && ICON="${ICON}.ico"
+        [ -e "${ICON}.png" ] && ICON="${ICON}.png"
+        sed -i -e "s;Icon=\([^\/]*\)\$;Icon=${ICON};" "${DST}"
+    fi
+done
+
+update-desktop-database ~/.local/share/applications
