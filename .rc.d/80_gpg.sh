db992c536d26ce1275835ca14f8de895da3e4f9a
Oliv5 <oliv5@caramail.com>
2023-01-17 22:28:00 +0100

    [rc] add gpg_agent_load_passphrase

--- .rc.d/80_gpg.sh
+++ .rc.d/80_gpg.sh
@@ -17,3 +17,40 @@ gpg_export_all() {
 	gpg --armor --export-secret-subkeys "$WHO" > "${NAME}.sec.sub.asc"
 	gpg --armor --gen-revoke "$WHO" > "${NAME}.rev.asc"
 }
+
+# Shortcut for gpg-preset-passphrase not in the path
+gpg-preset-passphrase() {
+	command "$(gpgconf --list-dirs libexecdir)"/gpg-preset-passphrase "$@"
+}
+
+# Ask one passphrase and register it in the agent
+# allow-preset-passphrase must be in ~/.gnupg/gpg-agent.conf
+# https://superuser.com/questions/1539189/gpg-preset-passphrase-caching-passphrase-failed-not-supported
+gpg_agent_load_passphrase() {
+	if ! grep allow-preset-passphrase "$HOME/.gnupg/gpg-agent.conf" >/dev/null; then
+		echo >&2 "Error: allow-preset-passphrase is not in ~/.gnupg/gpg-agent.conf ..."
+		return 1
+	fi
+	
+	# Ask passphrase
+	local PASSPHRASE
+	trap 'stty echo' INT
+	stty -echo
+	read -p "Passphrase: " PASSPHRASE
+	stty echo
+	trap - INT
+	echo
+
+	# Start agent and get key IDs
+	# Output looks like
+	#S KEYINFO B1A955E910AEFAAB2FAD9EADBFAA5C59AFAAF0AA D - - - P - - -
+	#S KEYINFO AAA2AAE20FCB9621D22BAFEE1C0AA2B011AA6AA6 D - - - P - - -
+	#OK
+	local KEYGRIP=
+	
+	# Loop over all keys and register the passphrase
+	local KEY
+	for KEY in $(gpg-connect-agent -q 'keyinfo --list' /bye | awk '/KEYINFO/ { print $3 }'); do
+		echo "$PASSPHRASE" | /usr/lib/gnupg/gpg-preset-passphrase --preset "$KEY"
+	done
+}
