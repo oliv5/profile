8cb90c29181b4e90384139e268354848240226f1
Oliv5 <4175562+oliv5@users.noreply.github.com>
2023-01-18 10:28:34 +0100

    [bin] update bootstrap script

--- bin/profile/bootstrap-profile.sh
+++ bin/profile/bootstrap-profile.sh
@@ -4,7 +4,7 @@ set -e
 
 ###################
 # Install a set of packages
-install_package() {
+install_packages() {
 	if apt --version >/dev/null 2>&1; then
 		sudo apt install "$@"
 	elif pkg --version >/dev/null 2>&1; then
@@ -15,38 +15,66 @@ install_package() {
 	fi
 }
 
+# Install from sources
+install_from_sources() {
+	local PACKAGE="${1:?No package specified...}"
+	local URL="${2:?No URL specified...}"
+	local DST="$HOME/.local/bin"
+	shift $(($# > 2 ? 2 : $#))
+
+	# Download sources
+	mkdir -p "$DST"
+	DST="$DST/_$PACKAGE"
+	if [ "${URL%.git}" != "$URL" ]; then
+		git clone --depth 1 "$URL" "$DST" ||
+			return 2
+	elif [ "${URL%.tgz}" != "$URL" ] || [ "${URL%.tar.gz}" != "$URL" ]; then
+		mkdir -p "$DST"
+		curl "$URL" | tar -xvz -C "$DST" ||
+			return 2
+	else
+		wget "$URL" -O "$DST" &&
+			chmod a+x "$DST" ||
+			return 2
+	fi
+
+	# Post-install
+	if [ $# -gt 0 ]; then
+		( cd "$DST"; for CMD; do eval "$CMD"; done )
+	fi
+
+	return 0
+}
+
 # Install program
 install() {
 	local CMD="${1:?No command specified...}"
 	local PACKAGE="${2:?No package specified...}"
 	local URL="$3"
-	local DST="${4:-$HOME/.local/bin}"
+	local DST="$HOME/.local/bin"
+
 	if command -v "$CMD" >/dev/null; then
 		echo >&2 "Warning: skip already-installed package $PACKAGE ..."
+		echo
 		return 0
 	fi
-	if ! install_package "$PACKAGE"; then
+
+	echo "Install package $PACKAGE ..."
+
+	if ! install_packages "$PACKAGE"; then
 		if [ -n "$URL" ]; then
-			mkdir -p "$DST"
-			DST="$DST/$PACKAGE"
-			if [ "${URL%.git}" != "$URL" ]; then
-				git clone --depth 1 "$URL" "$DST" ||
-					return 2
-			elif [ "${URL%.tgz}" != "$URL" ] || [ "${URL%.tar.gz}" != "$URL" ]; then
-				mkdir -p "$DST"
-				curl "$URL" | tar -xvz -C "$DST" ||
-					return 2
-			else
-				wget "$URL" -O "$DST" &&
-					chmod a+x "$DST" ||
-					return 2
-			fi
-			export PATH="$PATH:$DST"
+			shift 1
+			install_from_sources "$@"
 		else
 			echo >&2 "Warning: no clone url specified, skip package $PACKAGE ..."
+			echo
 			return 1
 		fi
 	fi
+	
+	echo "Done!"
+	echo
+
 	return 0
 }
 
@@ -67,6 +95,7 @@ Main() {
 	echo "HOME: $HOME"
 	echo "PRIVATE: $PRIVATE"
 	read -p "Continue? (enter/ctrl-c)" ANSWER
+	echo
 	mkdir -p "$HOME"
 	mkdir -p "$PRIVATE"
 
@@ -77,6 +106,7 @@ Main() {
 	install curl curl
 	install gpg gnupg
 	install gawk gawk
+	install make make
 	if [ "$(uname -p)" = "x86_64" ]; then
 		install git-annex git-annex https://downloads.kitenet.net/git-annex/linux/current/git-annex-standalone-amd64.tar.gz
 	elif [ "$(uname -m)" = "aarch64" ]; then
@@ -87,24 +117,21 @@ Main() {
 	install mr mr https://github.com/joeyh/myrepos.git
 	install vcsh vcsh https://github.com/RichiH/vcsh.git
 	install repo repo https://storage.googleapis.com/git-repo-downloads/repo
-	install fdfind fdfind https://github.com/sharkdp/fd
-	install rpl rpl https://github.com/kcoyner/rpl
-	install fzf fzf https://github.com/junegunn/fzf
+	install fdfind fd-find https://github.com/sharkdp/fd.git "install cargo cargo" "make"
+	install rpl rpl https://github.com/kcoyner/rpl.git "ln -s _rpl/rpl ../"
+	install fzf fzf https://github.com/junegunn/fzf.git "./install --no-fish --no-zsh"
 
 	# Clone profile repository
 	URL="https://github.com/oliv5/profile.git"
 	if [ "$PRIVATE" = "$HOME" ]; then
 		if ! vcsh list | grep profile >/dev/null; then
 			vcsh clone "$URL" profile
-			export PATH="$PRIVATE/profile/bin:$PATH"
 		else
 			echo >&2 "Warning: skip cloning existing profile repo ..."
 		fi
 	else
 		if ! [ -d "$PRIVATE/profile" ]; then
-			cd "$PRIVATE"
 			git clone "$URL" "$PRIVATE/profile"
-			export PATH="$PRIVATE/profile/bin:$PATH"
 		else
 			echo >&2 "Warning: skip cloning existing profile repo ..."
 		fi
@@ -112,4 +139,4 @@ Main() {
 }
 
 ###################
-Main "$@"
+(Main "$@")
