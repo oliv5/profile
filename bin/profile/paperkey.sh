5a0e5d1084011ef437d83d8fe0e62aa58d2aa569
Oliv5 <oliv5@caramail.com>
2023-01-18 16:59:52 +0100

    [bin] add paperkey.sh

---
+++ bin/profile/paperkey.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+# https://www.jabberwocky.com/software/paperkey/
+
+paperkey_print() {
+    for FILE; do
+        lp -o media=a4 -o page-bottom=36 -o page-left=36 -o page-right=36 -o page-top=36 -o sides=two-sided-long-edge "$FILE"
+    done
+}
+
+paperkey_print_all() {
+    echo "Print both paperkey of GPG secret keys + public keys as armoured ASCII"
+    paperkey_print *.paperkey *.pub.asc
+}
+
+# paperkey --secret-key my-secret-key.gpg --output to-be-printed.txt
+paperkey_backup() {
+    for FILE; do
+        paperkey --secret-key "$FILE" --output "${FILE%.*}.paperkey"
+    done
+}
+
+# paperkey --pubring my-public-key.gpg --secrets my-key-text-file.txt --output my-secret-key.gpg
+paperkey_restore() {
+    for FILE; do
+        paperkey --pubring "${FILE%.*}.pub.gpg" --secrets "${FILE%.*}.paperkey" --output "${FILE%.*}.sec.gpg"
+    done
+}
+
+# Main
+if [ $# -gt 1 ]; then
+    CMD="${1##paperkey_}"
+    shift
+    paperkey_$CMD "$@"
+fi
