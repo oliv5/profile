265e55391f707957ab59dfa9ba6b935435910320
Oliv5 <4175562+oliv5@users.noreply.github.com>
2023-01-24 23:57:32 +0100

    [bash] split bash/90_misc.sh + add arg_intersect() / arg_diff()

---
+++ .rc.d/bash/00_shell.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# http://tldp.org/LDP/abs/html/
+
+################################
+#http://www.tldp.org/LDP/abs/html/intandnonint.html
+
+# Returns true for interactive shells
+shell_isinteractive() {
+  [ -n "$PS1" ]
+}
+
+# Returns true for login shells
+shell_islogin() {
+  shopt -q login_shell
+}
+
+# Get script directory
+shell_pwd() {
+  echo $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+}
+
+################################
+# Check bashism in scripts
+bash_checkbashisms() {
+  command -v checkbashisms >/dev/null 2>&1 || die "checkbashisms not found..."
+  find "${1:-.}" -name "${2:-*.sh}" -exec sh -c 'checkbashisms {} 2>/dev/null || ([ $? -ne 2 ] && echo "checkbashisms {}")' \;
+}
+
+# Setup command completion
+bash_completion() {
+  complete -o bashdefault -o default -o nospace -F "$2" "$1" 2>/dev/null \
+  || complete -o default -o nospace -F "$2" "$1"
+}
+
+################################
+# Intersect 2 arguments lists
+arg_intersect() {
+  local L1="${1:?No list #1 specified...}"
+  local L2="${2:?No list #2 specified...}"
+	bash -c "sort <(echo $L1 | xargs -n1) <(echo $L2 | xargs -n1) | uniq -d"
+}
+
+#~ # Show differences between 2 arguments lists
+arg_diff() {
+  local L1="${1:?No list #1 specified...}"
+  local L2="${2:?No list #2 specified...}"
+	bash -c "sort <(echo $L1 | xargs -n1) <(echo $L2 | xargs -n1) | uniq -u"
+}
