e96c43e545158bbb3e203794e4f3596af1f88d24
Oliv5 <4175562+oliv5@users.noreply.github.com>
2023-04-28 23:49:58 +0200

    [gvim] add ultisnips/vim-snippets

---
+++ .config/vim/vim-legacy/.vim/bundle/ultisnips_install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# https://github.com/SirVer/ultisnips
+git clone https://github.com/SirVer/ultisnips.git ~/.vim/bundle/ultisnips
+# https://github.com/honza/vim-snippets
+git clone https://github.com/honza/vim-snippets ~/.vim/bundle/vim-snippets
