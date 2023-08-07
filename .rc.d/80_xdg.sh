a058ae4b5e2ed7f6a6c18d28444415518276ab88
Olivier Lanneluc <olivier.lanneluc@ubiik.com>
2023-08-07 23:20:52 +0200

    [rc] rm xdg desktop file

--- .rc.d/80_xdg.sh
+++ .rc.d/80_xdg.sh
@@ -18,8 +18,8 @@ xdg_autostart_show_all() {
 }
 
 xdg_mkshortcut() {
-    local EXEC="$(readlink -f ${1:?No path to executable defined...})"
-    local NAME="${2:-$(basename "$EXEC")}"
+    local EXEC="${1:?No command defined...}"
+    local NAME="${2:?No desktop file name specified...}"
     local ICON="$3"
     local TERMINAL="${4:-false}"
     local NODISPLAY="${5:-false}"
@@ -28,7 +28,6 @@ xdg_mkshortcut() {
     if [ -n "$6" ]; then
         DESKTOP="/etc/xdg/autostart/${NAME}.desktop"
         SUDO=sudo
-        
     fi
     $SUDO cat >"$DESKTOP" <<EOF
 [Desktop Entry]
@@ -44,16 +43,28 @@ EOF
     $SUDO sed -i '/^$/d' "$DESKTOP"
 }
 
+xdg_mkshortcut_cmd() {
+    local EXEC="${1:?No command defined...}"
+    shift
+    xdg_mkshortcut "/bin/sh -c '$EXEC'" "$@"
+}
+
+xdg_rmshortcut() {
+    local NAME="${1:?No desktop file name specified...}"
+    local DESKTOP="$HOME/.local/share/applications/${NAME}.desktop"
+    rm "$DESKTOP"
+}
+
 xdg_extract_appimage_icon() {
-    local EXEC="${1:?No path to executable defined...}"
+    local EXEC="${1:?No command defined...}"
     "$EXEC" --appimage-extract
     rsync -av squashfs-root/usr/share/icons/hicolor/ "$HOME/.local/share/icons/hicolor/"
     rm -r squashfs-root
 }
 
 xdg_set_appimage_icon() {
-    local EXEC="${1:?No path to executable defined...}"
-    local NAME="${2:-$(basename "$EXEC")}"
+    local EXEC="${1:?No command defined...}"
+    local NAME="${2:?No desktop file name specified...}"
     local DESKTOP="$HOME/.local/share/applications/${NAME}.desktop"
     local ICON="$HOME/.local/share/icons/hicolor/"
     xdg_extract_appimage_icon "$EXEC"
