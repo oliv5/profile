546b9f8288c0306f5b199b74b9b5eb3159ecbd0d
Olivier Lanneluc <olivier.lanneluc@ubiik.com>
2023-10-06 14:38:48 +0200

    [bin] find local usbreset

--- bin/profile/resources/usbreset/usbreset.sh
+++ bin/profile/resources/usbreset/usbreset.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 DEVICE="${1:?No usb device name to look for in 'lsusb'...}"
-lsusb | awk '/'"$DEVICE"'/ { print "/dev/bus/usb/" $2 "/" $4 }' | tr -d : | xargs -r -n1 sudo "$(command -v usbreset)"
+USBRESET="$(command -v usbreset || ls ./usbreset 2>/dev/null)"
+lsusb | awk '/'"$DEVICE"'/ { print "/dev/bus/usb/" $2 "/" $4 }' | tr -d : | xargs -r -n1 sudo "${USBRESET:?usbreset was not found...}"
