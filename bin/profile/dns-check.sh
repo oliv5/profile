230b8994fd6c4ab9743815898e796f309e7ef224
Olivier Lanneluc <olivier.lanneluc@ubiik.com>
2024-01-28 18:48:48 +0100

    [bin] update dns-check script

--- bin/profile/dns-check.sh
+++ bin/profile/dns-check.sh
@@ -1,24 +1,30 @@
 #!/bin/sh
 # From https://calomel.org/dns_verify.html
 NETS="$(ip addr | awk '/inet .*\/24/{print $2}' | grep -Fv 127.0.0.1 | xargs)"
-echo Analyse addresses: $NETS
-echo
-echo -e "\tip        ->     hostname      -> ip"
-echo '--------------------------------------------------------'  
+NETS_SKIPPED="$(ip addr | awk '/inet .*\/16/{print $2}' | grep -Fv 127.0.0.1 | xargs)"
+echo "Adresses queried: $NETS"
+echo "Adresses skipped: $NETS_SKIPPED"
+echo ""
+printf "     ip          -> hostname \t\t -> ip"
+echo ""
+echo "--------------------------------------------------------"
 for NET in $NETS; do
   NET="${NET%.*}"
   for n in $(seq 1 254); do
     A="${NET}.${n}"
-    HOST="$(dig -x "$A" +short)"
+    HOST="$(dig -x "$A" +short | xargs)"
     if test -n "$HOST"; then
-      ADDR="$(dig $HOST +short)"
+      ADDR="$(dig $HOST +short 2>/dev/null | xargs)"
       if test "$A" = "$ADDR"; then
-        echo -e "ok\t$A -> $HOST -> $ADDR"
+        printf "ok   $A -> $HOST\t -> $ADDR"
+      elif echo "$ADDR" | grep -F "$A" >/dev/null; then
+        printf "ok   $A -> $HOST\t -> [ $ADDR ]"
       elif test -n "$ADDR"; then
-        echo -e "fail\t$A -> $HOST -> $ADDR"
+        printf "fail $A -> $HOST\t -> $ADDR"
       else
-        echo -e "fail\t$A -> $HOST -> [unassigned]"
+        printf "fail $A -> $HOST\t -> [unassigned]"
       fi
+      echo ""
     fi
   done
 done
