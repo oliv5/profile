19b175e875666ea4a146c7de0bfa7df76e153ef1
Oliv5 <4175562+oliv5@users.noreply.github.com>
2024-05-14 11:40:53 +0200

    [rc] fix cpu_governor()

--- .rc.d/20_monitor.sh
+++ .rc.d/20_monitor.sh
@@ -21,7 +21,7 @@ alias dmesgg='dmesg | grep -i'
 # Get/set cpu governor
 cpu_governor() {
   local DEV="/sys/devices/system/cpu/cpu${2:-*}/cpufreq/scaling_governor"
-  if [ -n "$1" ]; then sudo sh -c "echo '$1' > '$DEV'"; else cat "$DEV"; fi
+  if [ -n "$1" ]; then sudo sh -c ":; for CPU; do echo '$1' > \$CPU; done" _ $DEV; else cat $DEV; fi
 }
 alias cpu_governors='cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_available_governors'
 alias cpu_powersave='cpu_governor powersave'
