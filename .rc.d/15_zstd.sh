25c6f1cfa182c0e73b5371e70b35abc21d2db86b
Olivier Lanneluc <olivier.lanneluc@ubiik.com>
2024-05-24 18:31:25 +0200

    [rc] add 15_zstd.sh

---
+++ .rc.d/15_zstd.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+###############################
+# Quick zstd compress/deflate
+zstdq() {
+  for SRC; do
+    if [ "$SRC" != "${SRC%.zst}" ]; then
+      zstdd "." "$SRC"
+    else
+      zstda "${SRC%%/*}.zst" "$SRC"
+    fi
+  done
+}
+
+# zstd compress
+zstda() {
+  local ARCHIVE="${1:?No archive to create...}"
+  shift 1
+  command zstd -z "$@" -o "$ARCHIVE"
+}
+
+# zstd deflate
+zstdd() {
+  local DST="${1:?No output directory specified...}"
+  local SRC
+  shift 1
+  mkdir -p "$DST"
+  for SRC; do
+    command zstd -d "$SRC" -o "$DST/$(basename "$SRC" .zst)"
+  done
+}
+
+########################################
+########################################
+# Last commands in file
+# Execute function from command line
+[ "${1#zstd}" != "$1" ] && "$@" || true
