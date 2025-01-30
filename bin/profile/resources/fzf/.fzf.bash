79b6e35a104646882bcf1f47f0e51bc67f5afcbf
Olivier Lanneluc <olivier.lanneluc@ubiik.com>
2025-01-30 14:52:27 +0100

    [bin] move resources in their own folder

--- bin/profile/resources/fzf/.fzf.bash
+++ bin/profile/resources/fzf/.fzf.bash
@@ -1,6 +1,6 @@
 # Setup fzf
 # ---------
-FZF_DIR="$RC_DIR/bin/profile/fzf"
+FZF_DIR="$RC_DIR/bin/profile/resources/fzf"
 if [[ ! "$PATH" == *"$FZF_DIR"/bin* ]]; then
   export PATH="${PATH:+${PATH}:}$FZF_DIR/bin"
 fi
