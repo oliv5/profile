4a29006cdffda027eb6fbf939267b002d804ed6c
Olivier Lanneluc <olivier.lanneluc@ubiik.com>
2025-04-18 23:08:52 +0200

    [screen] update config & aliases

--- .rc.d/98_screen.sh
+++ .rc.d/98_screen.sh
@@ -9,7 +9,6 @@ fi
 
 # Wrapper function
 screen() {
-  [ -z "$SCREEN_DISPLAY" ] && export SCREEN_DISPLAY="$DISPLAY"
   if [ $# = 0 ]; then
     # Recall old session or create a new one
     command -p screen -R
@@ -19,10 +18,52 @@ screen() {
   fi
 }
 
-# Send a command to a running screen
+# Check if screen session is running
+screen_is_running() {
+  command screen -ls "$1" >/dev/null 2>&1
+}
+
+# Run screen command (exec, quit, ...) in specific session
+screen_cmd() {
+  local SESSION="${1:?No session specified...}"
+  shift
+  command -p screen -S "$SESSION" -X "$@"
+}
+
+# Send a shell command to a running screen
 screen_send() {
-  local SESSION="${1:?No session specified...}"; shift $(min 1 $#)
+  local SESSION="${1:?No session specified...}"
+  shift
   command -p screen -S "$SESSION" -X stuff "^C\n${@}\n"
+}
+
+# Run a shell command in a new screen window
+screen_run() {
+  local SESSION="${1:?No session specified...}"
+  shift
+  if screen_is_running "$SESSION"; then
+    command -p screen -S "$SESSION" -X screen "$@"
+  else
+    command -p screen -S "$SESSION" -d -m "$@"
+  fi
+}
+
+# Run a shell command in loop; replace $N with the loop index starting from 1
+screen_loop() {
+  local N="${1:?No loop number specified...}"
+  N=$(($N)) # default 0
+  shift
+  echo "Stop with: screen -S loop -X quit"
+  for N in $(seq $N); do
+    eval screen_run loop "$@"
+  done
+}
+
+# Kill a session
+screen_kill() {
+  for S; do
+    command screen -S "$S" -X quit # -S before -X
+  done
 }
 
 # Set $DISPLAY
@@ -34,24 +75,18 @@ screen_setdisplay() {
 screen_ls() {
   command -p screen -q -ls
   if [ $? -ne 9 ]; then
-    screen -ls
+    command screen -ls
   fi
 }
 
 # Long aliases
 alias screen_recall='screen -r'
 alias screen_restore='screen -R -D'
-alias screen_quit='screen -X quit -S'
 alias screen_killdetached="screen -ls | awk -F. '/Detached/{print \$1}' | xargs -r kill"
-alias screen_killall="screen -ls | awk -F. '/[0-9]+\./{print \$1}' | xargs -r kill"
+alias screen_killall="screen -ls | awk -F. '/^\t/{print \$1}' | xargs -r kill"
 alias screen_clean='screen -wipe'
 alias screen_attach='reptyr'
-alias screen_fork='screen -d -m'
-
-# Short aliases
-alias scls='screen_ls'
-alias scr='screen_restore'
-alias sck='screen_quit'
+alias screen_detach='screen -d -m'
 
 # Autoload
 if [ -z "$STY" -a -z "$SCREEN_LOADED" ]; then
