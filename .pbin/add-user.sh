5ba119df4514e6efbd21b82d2f14f64c03b38243
Oliv5 <4175562+oliv5@users.noreply.github.com>
2025-06-06 09:34:27 +0200

    [bin] update add-user.sh

--- .pbin/add-user.sh
+++ .pbin/add-user.sh
@@ -31,12 +31,6 @@ else
 fi
 echo
 
-# Add user to dialout group
-if [ "$(question 'Add to the dialout group (y/n)? ')" = "y" ]; then
-    sudo usermod -a -G dialout "$USER"
-fi
-echo
-
 # Set SSH authorized key
 if [ "$(question 'Set SSH pubkey (y/n)? ')" = "y" ]; then
     sudo mkdir -p "/home/$USER/.ssh/"
@@ -46,15 +40,33 @@ if [ "$(question 'Set SSH pubkey (y/n)? ')" = "y" ]; then
 fi
 echo
 
-# Setup sudo
-for GRP in sudo admin group; do
-    if grep "$GRP" /etc/group >/dev/null; then
-        if [ "$(question 'Add user to the $GRP group (y/n)? ')" = "y" ]; then
+# Select user shell
+if [ "$(question 'Change user shell (y/n)? ')" = "y" ]; then
+    echo "Available shells:"
+    grep ^/bin /etc/shells
+    echo -n "Your choice: "
+    read CHOICE
+    if [ -f "$CHOICE" ] && [ -x "$CHOICE" ]; then
+        sudo usermod --shell "$CHOICE" "$USER"
+    fi
+fi
+
+# Add to groups
+for GRP in sudo admin lpadmin docker vboxusers wireshark dialout plugdev adm cdrom; do
+    if grep "^$GRP:" /etc/group >/dev/null; then
+        if [ "$(question 'Add user to the existing '"$GRP"' group (y/n)? ')" = "y" ]; then
+            sudo usermod -a -G "$GRP" "$USER"
+        fi
+    else
+        if [ "$(question 'Create group '"$GRP"' and add user into (y/n)? ')" = "y" ]; then
             sudo groupadd -f "$GRP"
             sudo usermod -a -G "$GRP" "$USER"
         fi
     fi
 done
+echo
+
+# Setup sudo
 for CMD in "/sbin/ip netns exec"; do
     if [ "$(question 'Allow user to run "'"$CMD"'" with sudo without password (y/n)? ')" = "y" ]; then
         cat <<-EOF | sudo EDITOR="tee -a" visudo -f "/etc/sudoers.d/75_$USER"
